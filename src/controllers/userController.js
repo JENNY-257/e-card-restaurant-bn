7b0ec57ac8816f00359cad819773381347ff12cb
JENNY-257 <j.iribagiza2@gmail.com>
2024-04-09 15:13:26 +0200

    fix user generate token after registration and login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -19,7 +19,10 @@ export const registerUser = async(req, res) => {
         const salt = await bcrypt.genSalt(10);
 
         const hashedPassword = await bcrypt.hash(password, salt);
-
+        const accessToken = jwt.sign(
+            {email:email},
+            process.env.JWT_SECRET,
+            {expiresIn:"1h"})
         const createUser = await User.create({
             firstName,
             lastName,
@@ -32,7 +35,7 @@ export const registerUser = async(req, res) => {
 
         return res.status(200).json({
             message:"user created succesfully please verify your email",
-            createUser
+            createUser, accessToken
         })
 
     } catch (error) {
@@ -50,10 +53,13 @@ try {
         return res.status(401).json({message:"user not found"});
     }
     const passwordMatch = await bcrypt.compare(password, user.password);
+    const token = jwt.sign({
+         email:user.email,
+         role:user.role,_id:user._id
+         }, process.env.JWT_SECRET,
+        {expiresIn:"1h"});
 
     if (passwordMatch) {
-      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
-
       return res.status(200).json({ message: 'Login successfully', token });
     } else {
       return res.status(401).json({ message: 'Invalid email or password' });
